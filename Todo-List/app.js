67e34dc44555b00a9f51a11f6a21a7c326ac4cb5
Nicholas <danielnick210@gmail.com>
2024-02-13 11:46:41 +0100

    counter app

--- Todo-List/app.js
+++ Todo-List/app.js
@@ -19,9 +19,13 @@ const addTask = () =>
 	input.value = "";
 	saveDate();
 }
+
+/** TRY AND SEPARATE THE FUNCTION FROM THE ADDEVENTLISTER METHOD JUST LIKE HOW I SAW IN THAT TUTORIAL. */
+
 listContainer.addEventListener("click", (e) => {
 	console.log("Clicked element tag name:", e.target.tagName);
-	if (e.target.tagName === 'LI') {
+    if (e.target.tagName === 'LI')
+    {
 		console.log("Clicked on LI");
 		e.target.classList.toggle("checked");
 		saveDate();
@@ -41,3 +45,5 @@ const showTask = () =>
 	listContainer.innerHTML = localStorage.getItem("data")
 }
 showTask();
+
+/* I WANT TO IMPLEMENT A FUCNTION THAT AFTER HITTING ENTER ON THE KEYBOARD IT WOULD AUTOMATICALLY ADD THE TASK TO THE LIST SO IT CAN SERVE AS A BUTTON ITSELF INSTEAD OF ALWAYS HITIING THE ADD BUTTON WHUCH CAN BE TIME CONSUMING AND STRESSFUL IN ITS SELF */
---
+++ counter/apps.js
@@ -0,0 +1,9 @@
+num_btn = document.getElementById("num-btn")
+count = 0
+function click()
+{
+    count += 1
+    console.log("clicked")
+}
+
+click()
